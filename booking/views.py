580f29a8671820b6e7294f8236c21afc284c618e
bijokmathew <bijo.nbr@hotmail.com>
2023-01-10 04:25:07 +0000

    changed static file path in base.html and added base.html path in to the urls.py for testing

--- booking/views.py
+++ booking/views.py
@@ -1,1 +1,8 @@
 from django.shortcuts import render
+from django.views import View, generic
+from .models import Booking
+
+
+class base(generic.ListView):
+    model = Booking
+    template_name = 'base.html'
