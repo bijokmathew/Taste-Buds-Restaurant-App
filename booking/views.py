61bbc310afcba4e6ffd44acee4697ade2a4e93b6
bijokmathew <bijo.nbr@hotmail.com>
2023-01-12 02:14:20 +0000

    Handled post method for booking form

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View, generic
 from .models import Booking
 from .forms import BookingForm
@@ -9,8 +9,26 @@ class Home(generic.ListView):
     template_name = 'index.html'
 
 
-def reserve_table(request):
-    context = {
-        'booking_from': BookingForm()
-    }
-    return render(request, 'booking.html', context=context)
+def reserve_table(request): 
+    """
+    * reserve_table method is used for booking the table
+    * If user select the booking table option then it goes to
+      booking form and user can update the required info.
+    * If user select submit on booking form then it saves the 
+      data to the booking model and redirect to bookinglist 
+    """
+    if request.method == 'GET':
+        context = {
+            'booking_from': BookingForm()
+        }
+        return render(request, 'booking.html', context=context)
+    elif request.method == 'POST':
+        form = BookingForm(data=request.POST)
+        if form.is_valid():
+            # To avoid Null constraint violation when saving object w
+            # ith Foreign Key relationships
+            booking_form = form.save(commit=False)
+            booking_form.user = request.user
+            booking_form.save()
+            return redirect('home')
+    
